3e50fb885647a4d5ec7dceb50c00fa543914b6b5
Carlotta Porcelli <carlottapor@gmail.com>
2021-06-07 11:39:35 +0200

    first exercise on variables

--- first_script.py
+++ first_script.py
@@ -12,4 +12,20 @@ print("I have", x, "DKK")
 
 y = x * 2
 
+# Formatted values
 
+first = "Carlotta"
+last = "Porcelli"
+
+name = "First Name: {}, Last Name: {}".format(first, last)
+name2 = f"First Name: {first}, Last Name: {last}"
+
+print(name)
+print(name2)
+
+decimal = 12.56345634534
+integer = int(decimal)
+print(integer)
+
+a = '12'
+float(a)
